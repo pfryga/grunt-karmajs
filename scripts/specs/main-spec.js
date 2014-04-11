d8361df917d9b6ae53b32f75026df9675ad43300
Piotr Fryga <piotr.fryga@allegro.pl>
2014-04-11 12:38:28 +0200

    zmiana main-spec z konfiguracja requirejs

--- scripts/specs/main-spec.js
+++ scripts/specs/main-spec.js
@@ -1,34 +1,17 @@
-var specs = [],
-    karmaStarted = false;
-    console.log('Karma Stop');
+var specs = [];
 
 for (var file in window.__karma__.files) {
     if (window.__karma__.files.hasOwnProperty(file)) {
         if (/Spec\.js$/.test(file)) {
-            console.log('pushing ' + file);
             specs.push(file);
         }
     }
 }
 
-console.log(specs);
-console.log(requirejs);
 requirejs.config({
-    // baseUrl: '/base/scripts/specs',
+    baseUrl: '/base/scripts/specs',
     deps: specs,
     callback: function () {
-        console.log('weszlo 1');
-        if (!karmaStarted) {
-            console.log('weszlo 2');
-            window.__karma__.start();
-            karmaStarted = true;
-            console.log('Karma Start');
-        }
+        window.__karma__.start();
     }
 });
-
-// require(['HelloWorldAlerter'], function(HelloWorldAlerter) {
-//     console.log('poszło');
-//     var alerter = new HelloWorldAlerter();
-//     alerter.run();
-// });
